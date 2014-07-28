99a24d93119d6061aba343d8eee0658fbef802c6
Nicolas Voirol <voirol.nicolas@gmail.com>
2014-07-28 15:12:03 +0200

    Added monadic-fors rule from wart remover

---
+++ sample-rules/src/MonadicFors.scala
@@ -0,0 +1,33 @@
+package com.typesafe.abide.sample
+
+import scala.tools.abide._
+import scala.tools.abide.traversal._
+
+class MonadicFors(val context : Context) extends WarningRule {
+  import context.universe._
+
+  val name = "monadic-fors"
+
+  case class Warning(tree : Tree) extends RuleWarning {
+    val pos = tree.pos
+    val message = "Monad doesn't depend on last binding, should use Applicative (see http://www.haskell.org/haskellwiki/Typeclassopedia)"
+  }
+
+  def check(enums : List[Tree]) {
+    val (syms, values) = enums.flatMap {
+      case fq"$bind <- $rhs" => Some(bind.symbol -> rhs)
+      case fq"$bind = $rhs" => Some(bind.symbol -> rhs)
+      case fq"if $cond" => None
+    }.unzip
+
+    for ((rhs, bound) <- values.tail zip syms.init if !rhs.exists(_.symbol == bound)) {
+      nok(Warning(rhs))
+    }
+  }
+
+  val step = optimize {
+    case t @ q"for (..$enums) yield $body" => println("fory="+t.getClass); check(enums)
+    case t @ q"for (..$enums) $body"       => println("for="+t.getClass); check(enums)
+  }
+}
+
