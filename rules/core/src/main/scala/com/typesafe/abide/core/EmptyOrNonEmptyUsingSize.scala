a45ac11c76a9910ff54fc5c1ef30776ea2820841
Johan Andrén <johan.andren@mejsla.se>
2014-12-10 18:06:01 +0000

    Using quasisquotes
    Not entirely sure it made it better though

--- rules/core/src/main/scala/com/typesafe/abide/core/EmptyOrNonEmptyUsingSize.scala
+++ rules/core/src/main/scala/com/typesafe/abide/core/EmptyOrNonEmptyUsingSize.scala
@@ -9,21 +9,18 @@ class EmptyOrNonEmptyUsingSize(val context: Context) extends WarningRule {
 
   val name = "empty-nonempty-using-size"
 
-  case class Warning(appl: Apply, empty: Boolean) extends RuleWarning {
+  case class Warning(appl: Tree, empty: Boolean) extends RuleWarning {
     val pos = appl.pos
     val message = "Traversable.size is very expensive on some collections, use " +
       (if (empty) "isEmpty"
       else "nonEmpty") + "which is O(1)"
   }
 
-  private val Size = TermName("size")
-  private val Length = TermName("length")
-  private val Equals = TermName("$eq$eq")
-  private val NotEquals = TermName("$bang$eq")
-
   val step = optimize {
-    case a @ Apply(Select(Select(t, Size | Length), op), List(Literal(Constant(0)))) if (op == Equals || op == NotEquals) && isTraversable(t) =>
-      nok(Warning(a, op == Equals))
+    case a @ q"$subj.size == 0" if isTraversable(subj)   => nok(Warning(a, empty = true))
+    case a @ q"$subj.length == 0" if isTraversable(subj) => nok(Warning(a, empty = true))
+    case a @ q"$subj.size != 0" if isTraversable(subj)   => nok(Warning(a, empty = false))
+    case a @ q"$subj.length != 0" if isTraversable(subj) => nok(Warning(a, empty = false))
   }
 
   private def isTraversable(tree: Tree) = tree.tpe <:< typeOf[Traversable[_]]
