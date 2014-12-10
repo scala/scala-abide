87c0943669a29bec4281f0531e9644c7bf968689
Johan Andrén <johan.andren@mejsla.se>
2014-12-10 18:13:10 +0000

    Big test split up in separate test cases

--- rules/core/src/test/scala/com/typesafe/abide/core/EmptyOrNonEmptyUsingSizeTest.scala
+++ rules/core/src/test/scala/com/typesafe/abide/core/EmptyOrNonEmptyUsingSizeTest.scala
@@ -6,32 +6,112 @@ class EmptyOrNonEmptyUsingSizeTest extends TraversalTest {
 
   val rule = new EmptyOrNonEmptyUsingSize(context)
 
-  "Using size or length == 0 on Traversables" should "give a warning" in {
+  "Using size == 0 on List" should "give a warning" in {
     val tree = fromString("""
       class Test {
         List().size == 0
-        List().length == 0
-        Seq().size == 0
-        Seq().length == 0
-        Set().size == 0
-        Map().size == 0
       } """)
 
-    global.ask { () => apply(rule)(tree).size should be(6) }
+    global.ask { () => apply(rule)(tree).nonEmpty should be(true) }
   }
 
-  "Using size or length != 0 on Traversables" should "give a warning" in {
+  "Using size != 0 on List" should "give a warning" in {
     val tree = fromString("""
       class Test {
         List().size != 0
+      } """)
+
+    global.ask { () => apply(rule)(tree).nonEmpty should be(true) }
+  }
+
+  "Using length == 0 on List" should "give a warning" in {
+    val tree = fromString("""
+      class Test {
+        List().length == 0
+      } """)
+
+    global.ask { () => apply(rule)(tree).nonEmpty should be(true) }
+  }
+
+  "Using length != 0 on List" should "give a warning" in {
+    val tree = fromString("""
+      class Test {
         List().length != 0
+      } """)
+
+    global.ask { () => apply(rule)(tree).nonEmpty should be(true) }
+  }
+
+  "Using size == 0 on Seq" should "give a warning" in {
+    val tree = fromString("""
+      class Test {
+        Seq().size == 0
+      } """)
+
+    global.ask { () => apply(rule)(tree).nonEmpty should be(true) }
+  }
+
+  "Using size != 0 on Seq" should "give a warning" in {
+    val tree = fromString("""
+      class Test {
         Seq().size != 0
+      } """)
+
+    global.ask { () => apply(rule)(tree).nonEmpty should be(true) }
+  }
+
+  "Using length == 0 on Seq" should "give a warning" in {
+    val tree = fromString("""
+      class Test {
+        Seq().length == 0
+      } """)
+
+    global.ask { () => apply(rule)(tree).nonEmpty should be(true) }
+  }
+
+  "Using length != 0 on Seq" should "give a warning" in {
+    val tree = fromString("""
+      class Test {
         Seq().length != 0
+      } """)
+
+    global.ask { () => apply(rule)(tree).nonEmpty should be(true) }
+  }
+
+  "Using size == 0 on Set" should "give a warning" in {
+    val tree = fromString("""
+      class Test {
+        Set().size == 0
+      } """)
+
+    global.ask { () => apply(rule)(tree).nonEmpty should be(true) }
+  }
+
+  "Using size != 0 on Set" should "give a warning" in {
+    val tree = fromString("""
+      class Test {
         Set().size != 0
+      } """)
+
+    global.ask { () => apply(rule)(tree).nonEmpty should be(true) }
+  }
+
+  "Using size == 0 on Map" should "give a warning" in {
+    val tree = fromString("""
+      class Test {
+        Map().size == 0
+      } """)
+
+    global.ask { () => apply(rule)(tree).nonEmpty should be(true) }
+  }
+
+  "Using size != 0 on Map" should "give a warning" in {
+    val tree = fromString("""
+      class Test {
         Map().size != 0
       } """)
 
-    global.ask { () => apply(rule)(tree).size should be(6) }
+    global.ask { () => apply(rule)(tree).nonEmpty should be(true) }
   }
 
   "Using size or length == 0 on objects not subclasses to Traversable" should "not give a warning" in {
